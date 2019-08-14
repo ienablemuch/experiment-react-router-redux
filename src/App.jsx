4b93c65cd820a1082d689b3c785959b16fe05809
dev <me@ienablemuch.com>
2019-08-14 15:41:00 +0800

    Integrate redux with react-router by using connected-react-router

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./App.css";
+
+import { Route, Switch } from "react-router";
+
+import { Link } from "react-router-dom";
+
+import { connect } from "react-redux";
+
+function App({ pathname, search, hash }) {
+    return (
+        <div className="App">
+            <table>
+                <tr>
+                    <td>Pathname</td>
+                    <td>{pathname}</td>
+                </tr>
+                <tr>
+                    <td>Search</td>
+                    <td>{search}</td>
+                </tr>
+                <tr>
+                    <td>Hash</td>
+                    <td>{hash}</td>
+                </tr>
+            </table>
+            <Link to="/">Home</Link> | <Link to="/About">About</Link> |{" "}
+            <Link to="/qwerty">Qwerty</Link>
+            <Switch>
+                <Route exact path="/" render={() => <div>Home</div>} />
+                <Route exact path="/about" render={() => <div>About</div>} />
+                <Route render={() => <div>Miss</div>} />
+            </Switch>
+        </div>
+    );
+}
+
+const mapStateToProps = state => ({
+    pathname: state.router.location.pathname,
+    search: state.router.location.search,
+    hash: state.router.location.hash
+});
+
+export default connect(mapStateToProps)(App);
