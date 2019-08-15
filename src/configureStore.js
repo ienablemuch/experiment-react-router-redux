acf04ffe4b1ac72197cf45e1c83d5d840747bd85
dev <me@ienablemuch.com>
2019-08-15 10:50:10 +0800

    prefer explicit over default. it's refactor-friendly

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,7 +2,7 @@ import { createBrowserHistory } from "history";
 import { applyMiddleware, compose, createStore } from "redux";
 import { routerMiddleware } from "connected-react-router";
 
-import createRootReducer from "./reducers";
+import { createRootReducer } from "./reducers";
 
 export const history = createBrowserHistory();
 
